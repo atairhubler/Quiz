7ec5794e837c3fa034a1d76cf0130a3fa0abe288
Atair Hubler Junior <atairjrbr@gmail.com>
2024-08-12 13:20:18 -0300

    Adicionar arquivos de projeto.

---
+++ Quiz/Program.cs
@@ -0,0 +1,11 @@
+﻿//Crie um programa que implemente um quiz simples de perguntas e respostas. Utilize um dicionário para armazenar as perguntas e as respostas corretas.
+
+
+// Criar dicionario
+// Atribuir valores ao dicionário / Pergunta e Resposta.
+// Criar o menu para iniciar o Quiz e apresentar perguntas esperando resposta.
+
+Dictionary<string, int> Quiz = new Dictionary<string, string>();
+
+
+Quiz.Add ("Qual a cor do ceu?", "Azul");
